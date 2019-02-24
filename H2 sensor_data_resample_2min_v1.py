487acdf88a8d23d1b88e0644763b824d6e6baf6a
CaptainZZZ <33072544+CaptainZZZ@users.noreply.github.com>
2019-02-24 18:23:25 -0500

    Add files via upload

---
+++ H2 sensor_data_resample_2min_v1.py
@@ -0,0 +1,52 @@
+from openpyxl import Workbook
+
+#Input file names
+files=[]
+while True:
+	files.append(input('File name?\n'))
+	if files[-1]=='':
+		break
+
+#Setup workbook
+wb=Workbook()
+ws=wb.active
+
+j=1
+items=['Time,s','T,hrs','R','G','B','T','RH']
+for item in items:
+	ws.cell(row=1,column=j,value=item)
+	j=j+1
+
+#Define resample function
+def resample(file_name,offset):
+	n=0
+	f=open(file_name,mode='r')
+	for line in f:
+		i=0
+		l=line
+		l=l.split('\t')
+		if l[0]=='X_Value':
+			n=1
+			continue
+		if n==1:
+			t0=float(l[0])
+		if n>=1 and n%24==0:
+			t1=float(l[0])
+			t=(t1-t0)/3600
+			l.insert(1,t)
+			for item in l:
+				ws.cell(row=n//24+1,column=i+offset,value=float(item))
+				i=i+1
+		if n>=1:
+			n=n+1
+	return len(l)+1
+	f.close()
+
+k=0
+for file in files:
+        try:
+        	print(k)
+        	k=k+resample(file,k+1)
+        except:
+        	pass
+wb.save(files[0]+'_resampled.xlsx')
